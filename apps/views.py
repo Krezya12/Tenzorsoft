a5ab9c4f51ca5fc52bb9737669502794fc56baee
Krezya12 <zieratzierat346@gmail.com@gmail.com>
2026-06-22 12:06:42 +0500

    5

--- apps/views.py
+++ apps/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def contact_view(request):
+    return render(request, '')
+
+def portfolio_view(request):
+    return render(request, '')
+
