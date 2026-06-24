f41a73679469eb91cd4edaf4ca9aab27f6cba6bf
Krezya12 <zieratzierat346@gmail.com@gmail.com>
2026-06-24 11:21:29 +0500

    9

--- tenzorsoft/urls.py
+++ tenzorsoft/urls.py
@@ -1,13 +1,18 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 from tenzorsoft import settings
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+]
+
+urlpatterns += i18n_patterns(
     # path('admin/', admin.site.urls),
     path('', include('apps.urls')),
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
